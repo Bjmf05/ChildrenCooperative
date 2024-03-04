369fa72c259cef493461d9275af0f1fd9eb74c12
Breiner Muñoz Fallas <breiner.munoz.fallas@est.una.ac.cr>
2024-03-03 18:59:52 -0600

    changes in CSS and SCENEBUILDER

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module cr.ac.una.tareaprogra {
     requires javafx.controls;
     requires javafx.fxml;
-
+	requires java.logging;	
     opens cr.ac.una.tareaprogra to javafx.fxml;
     exports cr.ac.una.tareaprogra;
     requires javafx.graphicsEmpty;
