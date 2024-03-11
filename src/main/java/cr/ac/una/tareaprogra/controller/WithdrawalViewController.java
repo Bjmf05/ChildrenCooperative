7786ee33c1bd0c7611c74bf95b4b618bd480730c
MarconiCalvo <marconi.calvo.campos@est.una.ac.cr>
2024-03-10 22:37:14 -0600

    Aceept View

--- src/main/java/cr/ac/una/tareaprogra/controller/WithdrawalViewController.java
+++ src/main/java/cr/ac/una/tareaprogra/controller/WithdrawalViewController.java
@@ -6,7 +6,12 @@ package cr.ac.una.tareaprogra.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,6 +20,19 @@ import javafx.fxml.Initializable;
  */
 public class WithdrawalViewController implements Initializable {
 
+    @FXML
+    private TextField txfVerify;
+    @FXML
+    private Button btnVerify;
+    @FXML
+    private ComboBox<?> txfAccount;
+    @FXML
+    private TextField txfAmountToWithdraw;
+    @FXML
+    private Button btnCancel;
+    @FXML
+    private Button btnWithdrawal;
+
     /**
      * Initializes the controller class.
      */
@@ -22,5 +40,17 @@ public class WithdrawalViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void onActionBtnVerify(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnCancel(ActionEvent event) {
+    }
+
+    @FXML
+    private void onActionBtnWithdrawal(ActionEvent event) {
+    }
     
 }
