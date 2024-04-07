d9edff5488df16fc921a6cd17c30197840b8c142
MarconiCalvo <marconi.calvo.campos@est.una.ac.cr>
2024-04-06 23:56:03 -0600

    Generate PDF

--- src/main/java/cr/ac/una/tareaprogra/model/PrintPdf.java
+++ src/main/java/cr/ac/una/tareaprogra/model/PrintPdf.java
@@ -1,98 +1,95 @@
-//package cr.ac.una.tareaprogra.model;
-//
-//import com.itextpdf.io.image.ImageDataFactory;
-//import com.itextpdf.kernel.colors.Color;
-//import com.itextpdf.kernel.colors.DeviceRgb;
-//import com.itextpdf.kernel.font.PdfFont;
-//import com.itextpdf.kernel.font.PdfFontFactory;
-//import com.itextpdf.kernel.geom.PageSize;
-//import com.itextpdf.kernel.pdf.PdfDocument;
-//import com.itextpdf.kernel.pdf.PdfWriter;
-//import com.itextpdf.layout.Document;
-//import com.itextpdf.layout.Style;
-//import com.itextpdf.layout.element.Image;
-//import com.itextpdf.layout.element.Paragraph;
-//import com.itextpdf.layout.element.Text;
-//import com.itextpdf.layout.properties.TextAlignment;
-//import java.awt.Desktop;
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.io.IOException;
-//import java.time.LocalDate;
-//
-///**
-// *
-// * @author PC
-// */
-//public class PrintPdf {
-//
-//    private int id = 604700092;
-//    private String name = "Breiner Munoz Fallas";
-//    private String invoice = "M0511";
-//    private LocalDate dateOfBirth = LocalDate.of(2001, 11, 05);
-//    private String sex = "Masculino";
-//    private String addressPhoto = "src/main/resources/cr/ac/una/tareaprogra/resources/IconChildPhoto.png";
-//    private String title = "Coopetoy";
-//    private String logo = "src/main/resources/cr/ac/una/tareaprogra/resources/logo.png";
-//    private String crab = "src/main/resources/cr/ac/una/tareaprogra/resources/crab.png";
-//    private String octopus = "src/main/resources/cr/ac/una/tareaprogra/resources/octopus.png";
-//    private String InriaSerif = "src/main/resources/cr/ac/una/tareaprogra/resources/InriaSerif-Light.ttf";
-//    private String Regular = "src/main/resources/cr/ac/una/tareaprogra/resources/Rancho-Regular.ttf";
-//    private Color backgroundColor = new DeviceRgb(106, 202, 216);
-//    private Color titleColor = new DeviceRgb(30, 43, 105);
-//    private Style titleStyle = new Style().setFontSize(45).setBold().setFontColor(titleColor).setTextAlignment(TextAlignment.CENTER);
-//    private Style paragraphText = new Style().setFontSize(20).setTextAlignment(TextAlignment.LEFT);
-//
-//    public void printAsociate() throws FileNotFoundException, IOException {
-//        File file = new File(invoice + ".pdf");
-//
-//        PdfDocument pdfDocument = new PdfDocument(new PdfWriter(file));
-//        Document document = new Document(pdfDocument, new PageSize(500, 400));
-//        document.setBackgroundColor(backgroundColor);
-//
-//        PdfFont font = PdfFontFactory.createFont(Regular);
-//        titleStyle.setFont(font);
-//        paragraphText.setFont(font);
-//        Paragraph prueba = new Paragraph().add("prueba");
-//        Paragraph paragraphTitle = new Paragraph().add(title + "\n");
-//        Paragraph paragraphAsociate = new Paragraph()
-//                .add(new Text("Cedula del Cliente: ").setBold())
-//                .add(id + "\n")
-//                .add(new Text("Nombre del Cliente: ").setBold())
-//                .add(name + "\n")
-//                .add(new Text("Folio del Cliente: ").setBold())
-//                .add(invoice + "\n")
-//                .add(new Text("Fecha Nacimiento del Cliente: ").setBold())
-//                .add(dateOfBirth + "\n")
-//                .add(new Text("Sexo del Cliente: ").setBold())
-//                .add(sex + "\n");
-//
-//        Image imagePhoto = new Image(ImageDataFactory.create(addressPhoto));
-//        imagePhoto.scaleToFit(140, 140);
-//        imagePhoto.setFixedPosition(325, 155);
-//
-//        Image imageLogo = new Image(ImageDataFactory.create(logo));
-//        imageLogo.scaleToFit(50, 50);
-//        imageLogo.setFixedPosition(25, 325);
-//
-//        Image imageCrab = new Image(ImageDataFactory.create(crab));
-//        imageCrab.scaleToFit(140, 140);
-//        imageCrab.setFixedPosition(18, -5);
-//
-//        Image imageOctopus = new Image(ImageDataFactory.create(octopus));
-//        imageOctopus.scaleToFit(170, 170);
-//        imageOctopus.setFixedPosition(315, 10);
-//
-//        paragraphTitle.addStyle(titleStyle);
-//        paragraphAsociate.addStyle(paragraphText);
-//
-//        document.add(paragraphTitle);
-//        document.add(imageLogo);
-//        document.add(paragraphAsociate);
-//        document.add(imagePhoto);
-//        document.add(imageCrab);
-//        document.add(imageOctopus);
-//        document.close();
-//        Desktop.getDesktop().open(file);
-//    }
-//}
+package cr.ac.una.tareaprogra.model;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.pdf.BaseFont;
+import cr.ac.una.tareaprogra.App;
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+/**
+ *
+ * @author Marconi
+ */
+public class PrintPdf {
+    
+    private Long id = 118720125L;
+    private String name = "Marconi Calvo Campos";
+    private String invoice = "C019";
+    private LocalDate dateOfBirth = LocalDate.of(2003, 03, 19);
+    private String sex = "Masculino";
+    private String title = "Coopetoy";  
+    private String addressPhoto = "resources/IconChildPhoto.png";
+    private String logo = "resources/logo.png";
+    private String crab = "resources/crab.png";
+    private String octopus = "resources/octopus.png";
+    private String InriaSerif = "cr/ac/una/tareaprogra/resources/InriaSerif-Light.ttf";
+    private String Regular = "cr/ac/una/practica/resources/Rancho-Regular.ttf";
+
+    public void printPdf()throws IOException, DocumentException{
+        File file = new File(invoice + ".pdf");
+        com.itextpdf.text.Rectangle pageSize = new com.itextpdf.text.Rectangle(500f, 400f);
+        Document document = new Document(pageSize);
+        BaseFont baseFont = BaseFont.createFont(Regular,BaseFont.IDENTITY_H,BaseFont.EMBEDDED);
+        
+        PdfWriter.getInstance(document, new FileOutputStream(file));
+        
+        Image imagePhoto = Image.getInstance(App.class.getResource(addressPhoto));
+        imagePhoto.scaleToFit(140, 140);
+        imagePhoto.setAbsolutePosition(325, 155);
+            
+        Image imageLogo = Image.getInstance(App.class.getResource(logo));
+        imageLogo.scaleToFit(50, 50);
+        imageLogo.setAbsolutePosition(25, 325);
+        
+        Image imageCrab = Image.getInstance(App.class.getResource(crab));
+        imageCrab.scaleToFit(140, 140);
+        imageCrab.setAbsolutePosition(18, -5);
+
+        Image imageOctopus = Image.getInstance(App.class.getResource(octopus));
+        imageOctopus.scaleToFit(170, 170);
+        imageOctopus.setAbsolutePosition(315, 10);
+        
+        Font font = new Font(baseFont, 20, Font.NORMAL , BaseColor.BLACK);
+        Font titleFont = new Font(baseFont, 48, Font.NORMAL, BaseColor.BLUE);
+        
+        document.open();
+        Paragraph titleParagraph = new Paragraph();
+        titleParagraph.setAlignment(Paragraph.ALIGN_CENTER);
+        titleParagraph.setFont(titleFont);
+        titleParagraph.add(title+"\n\n\n");
+        
+        document.add(titleParagraph);
+        document.add(new Paragraph("Cedula del Cliente: " + id, font));
+        document.add(new Paragraph("Nombre del Cliente: " + name, font));
+        document.add(new Paragraph("Folio del Cliente: " + invoice, font));
+        document.add(new Paragraph("Fecha Nacimiento del Cliente: " + dateOfBirth, font));
+        document.add(new Paragraph("Sexo del Cliente: " + sex, font));
+        
+        document.add(imagePhoto);
+        document.add(imageLogo);
+        document.add(imageCrab);
+        document.add(imageOctopus);
+        
+        document.close();
+        Desktop.getDesktop().open(file); 
+    }
+    
+    void fillDataPdf(Associate associate) throws IOException, DocumentException{
+        id = associate.getId();
+        name = associate.getName();
+        invoice = associate.getInvoice();
+        dateOfBirth = associate.getDateOfBirth();
+        sex = associate.getSex();
+        addressPhoto = associate.getAdressPhoto();
+        printPdf();
+    }
+}
